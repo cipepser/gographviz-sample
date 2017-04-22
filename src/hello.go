8c1c5668a9f10df75e18000c65aecd260ba49932
cipepser <respepic@gmail.com>
2017-04-22 14:45:01 +0900

    add hello.go

---
+++ src/hello.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/awalterschulze/gographviz"
+)
+
+func main() {
+	g := gographviz.NewGraph()
+	if err := g.SetName("G"); err != nil {
+		panic(err)
+	}
+	if err := g.SetDir(true); err != nil {
+		panic(err)
+	}
+	if err := g.AddNode("G", "Hello", nil); err != nil {
+		panic(err)
+	}
+	if err := g.AddNode("G", "World", nil); err != nil {
+		panic(err)
+	}
+	if err := g.AddEdge("Hello", "World", true, nil); err != nil {
+		panic(err)
+	}
+	s := g.String()
+	fmt.Println(s)
+}
